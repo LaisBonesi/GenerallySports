3f67f6fd0671a6a462641245b89aee6f58bcb00f
Your Name <laisbonesi@gmail.com>
2021-10-17 13:10:30 -0300

    first commit

---
+++ DAO/DAO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GenerallySports.DAO
+{
+    public class DAO
+    {
+        public string connectionString = @"server=DESKTOP-US4LHLF\SQLEXPRESS; database=GENERALLY;integrated security=yes;";
+
+        public string ConnString
+        {
+            get
+            {
+                return this.connectionString;
+            }
+        }
+    }
+}
+
---
+++ Models/Fornecedor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GenerallySports.Models
+{
+    public partial class Fornecedor : Endereco
+    {
+        public int Id { get; set; }
+        public string Razaosocial { get; set; }
+        public string Cnpj { get; set; }
+        public string Telefone { get; set; }
+        public int IdEndereco { get; set; }
+        public string Email { get; set; }
+        public string Categoriaproduto { get; set; }
+        public string Ativo
+        {
+            get; set;
+        }
+    }
+}
